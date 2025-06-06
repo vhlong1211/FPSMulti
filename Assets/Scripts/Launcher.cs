6872b0328d3f4f15ea447fb82c7e2b73523a5c52
LongVh-Grus <126643181+LongVh-Grus@users.noreply.github.com>
2025-06-06 11:59:43 +0700

    backup

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,73 @@
+using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    public static Launcher ins;
+    private void Awake()
+    {
+        ins = this;
+    }
+
+    public GameObject loadingScreen;
+    public TMP_Text loadingText;
+    public GameObject menuBtns;
+
+    public GameObject createRoomScreen;
+    public TMP_InputField roomNameInput;
+
+    private void Start()
+    {
+        CloseMenus();
+        loadingScreen.SetActive(true);
+        loadingText.text = "Connecting to network...";
+
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    void CloseMenus()
+    {
+        loadingScreen.SetActive(false);
+        menuBtns.SetActive(false);
+        createRoomScreen.SetActive(false);
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+
+        loadingText.text = "Joinin Lobby...";
+    }
+
+    public override void OnJoinedLobby()
+    {
+        CloseMenus();
+        menuBtns.SetActive(true);
+    }
+
+    public void OnClickCreateRoom()
+    {
+        CloseMenus();
+        createRoomScreen.SetActive(true);
+    }
+
+    public void OnClickCreateRoomReal()
+    {
+        if (string.IsNullOrEmpty(roomNameInput.text))
+        {
+            return;
+        }
+        RoomOptions options = new RoomOptions();
+        options.MaxPlayers = 8;
+        PhotonNetwork.CreateRoom(roomNameInput.text, options);
+
+        CloseMenus();
+        loadingText.text = "Creating Room...";
+        loadingScreen.SetActive(true);
+
+    }
+}
